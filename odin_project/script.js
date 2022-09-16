7b8777432012e9286d658b508d5051e7cd42a998
Pato <patrik.potocny85@gmail.com>
2022-09-16 20:30:46 +0200

    Odin priject

---
+++ odin_project/script.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+const theHobbit = new Book('Hobbit', 'JRR', 295, 'not read yet')
+console.log(theHobbit.info())
+
