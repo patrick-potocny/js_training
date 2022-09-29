67cd692b4f306b4b016340dfbf6708cea0f2ec77
patrick-potocny <pato.potocny85@gmail.com>
2022-09-29 15:59:49 +0200

    Working on library app

--- library/script.js
+++ library/script.js
@@ -1,28 +1,61 @@
 let myLibrary = [];
 
-function Book(title, author, read) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.read = read
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.read}.`
+    this.pages = pages
+    this.updateRead = function () {
+        if (this.read === 'YES'){
+            this.read = 'NO'
+        }else {
+            this.read = 'YES'
+        }
     }
 }
 
-function addBookToLibrary(title, author, read) {
-    const newBook = new Book(title, author, read);
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-addBookToLibrary('Book1', 'author1', 'read');
-addBookToLibrary('Book2', 'author2', 'not read');
-addBookToLibrary('Book3', 'author3', 'read');
+addBookToLibrary('Book1', 'author1', '333', 'YES');
+addBookToLibrary('Book2', 'author2', '333', 'NO');
+addBookToLibrary('Book3', 'author3', '333', 'YES');
+addBookToLibrary('Book4', 'author4', '444', 'NO');
 
-const booksElement = document.getElementById('books')
 
-function populateLibrary(params) {
-    for (const book of myLibrary) {
-        booksElement.
+function updateBooks(library) {
+    const booksElement = document.getElementById('books-grid')
+    booksElement.innerHTML = ''
+    for (bookIndex in library){
+        book = library[bookIndex]
+        booksElement.innerHTML += `
+        <div class="card">
+            <h3 class="title">${book.title}</h3>
+            <p class="author">${book.author}</p>
+            <p class="pages">Number of pages: <span>${book.pages}</span></p>
+            <div class="read-book">
+                <p class="read-p">Read the book: </p>
+                <button class="read-btn" data-book-index="${bookIndex}" onclick=updateRead(this)>${book.read}</button>
+            </div>
+            <button class="remove-book" data-book-index="${bookIndex}" onclick=removeBook(this)>Remove Book</button>
+        </div>`
     }
 }
+
+function updateRead(element) {
+    myLibrary[element.dataset.bookIndex].updateRead()
+    updateBooks(myLibrary)
+}
+
+function removeBook(element) {
+    myLibrary.splice(element.dataset.bookIndex, 1)
+    updateBooks(myLibrary)
+}
+
+updateBooks(myLibrary)
+
+
+
 
