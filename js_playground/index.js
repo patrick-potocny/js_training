5ada009fa9501609000ee49e02aa264443d65a90
patrick-potocny <pato.potocny85@gmail.com>
2022-10-21 16:24:45 +0200

    training async api calls

--- js_playground/index.js
+++ js_playground/index.js
@@ -1,3 +1,12 @@
 API_KEY = '8ZDWCzqq7DLLVy9klpx8pwNxRQvjVt3y'
-url = `api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`
-console.log(url);
+url = `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`
+const image = document.getElementsByClassName('image')[0]
+const sec_img = document.getElementById('image')
+
+fetch(url, {mode: 'cors'})
+.then((response) => response.json())
+.then((data) => image.src = data.data.images.original.url);
+
+fetch(url, {mode: 'cors'})
+.then((response) => response.json())
+.then((data) => sec_img.src = data.data.images.original.url);
